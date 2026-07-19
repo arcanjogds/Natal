62d9e11e70f3f61eafd16b698c43b203516657cc
Guilherme Arcanjo <guilhermearcanjodasilva@gmail.com>
2026-07-18 22:18:21 -0300

    Allow localhost in CORS origin along with frontend URL

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(helmet({
 }));
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: [process.env.FRONTEND_URL, 'http://localhost:5173'].filter(Boolean),
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 app.use(express.json({ limit: '10kb' }));
